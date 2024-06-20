6472c739093aa2e05929516ed2902caf0088afe1
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-20 14:14:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+Car.prototype.getMakeModel=function(){
+	return `${this.make} ${this.model}`
+}
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed=topSpeed;
+}
+// Inherit the Car prototype in the SportsCar prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
 
-function SportsCar(make, model, topSpeed) {}
-
+// Adding getTopSpeed method to the SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
